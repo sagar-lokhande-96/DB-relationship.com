d502a8bb84ee0f8ca384d8b3f4284386cd58b133
Sagar Lokhande <sagarlokhande159@gmail.com>
2025-04-26 14:41:54 +0530

    first commit / initialization of database

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+
+main()
+    .then(() =>{
+        console.log("MongoDB Connected!");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/RelationDemo');
+}
+
+  
